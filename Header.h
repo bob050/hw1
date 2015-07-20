6730cf8cd900c6f24277e1057b11473857d7a15d
bob050 <woousarang@mail.com>
2015-07-20 22:05:12 +0900

    homework1
    homework1 Project

--- Header.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <memory>
-#include <strsafe.h>
-#include <stdlib.h>
-
-void print(_In_ const char* fmt, _In_ ...);
---
+++ OS/OS/Header.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <memory>
+#include <strsafe.h>
+#include <stdlib.h>
+
+void print(_In_ const char* fmt, _In_ ...);
